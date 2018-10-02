e367fbe0f0e73ec5d9cfc6656632329a889fa391
Matthew Szekalski <mattszekalski@icloud.com>
2018-10-01 21:41:43 -0400

    added store and changed index file

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,25 @@
 /** @format */
 
 import { AppRegistry } from "react-native";
-import App from "./components/App";
-import { name as appName } from "./app.json";
+// import App from "./components/App";
+// import { name as appName } from "./app.json";
 
-AppRegistry.registerComponent(appName, () => App);
+import React, { Component } from "react";
+import { configureStore } from "./redux/store";
+import { Provider } from "react-redux";
+
+import App from "./App.container";
+
+const store = configureStore();
+
+class KRoberts extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  }
+}
+
+AppRegistry.registerComponent("KRoberts", () => KRoberts);
