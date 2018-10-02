4545af89df8b71bd3c8f07f67e642bdbf166d972
Matthew Szekalski <mattszekalski@icloud.com>
2018-10-01 22:16:23 -0400

    fixed index

--- frontend/index.js
+++ frontend/index.js
@@ -1,14 +1,13 @@
 /** @format */
 
 import { AppRegistry } from "react-native";
-// import App from "./components/App";
 // import { name as appName } from "./app.json";
 
 import React, { Component } from "react";
 import { configureStore } from "./store/store";
 import { Provider } from "react-redux";
 
-import App from "./App";
+import App from "./components/App";
 
 const store = configureStore();
 
